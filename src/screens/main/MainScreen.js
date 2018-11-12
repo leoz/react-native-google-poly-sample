0085ebac0323d53c5b597ff8d7abf0ea1d5acaf8
Leonid Zolotarev <leoz@yahoo.com>
2018-11-12 14:28:38 -0500

    Remove obsolete files

--- src/screens/main/MainScreen.js
+++ src/screens/main/MainScreen.js
@@ -1,24 +1,19 @@
 //
-import React from 'react';
-import { Button } from 'react-native';
-import MainView from './ARView';
+import React from "react";
+import { Button } from "react-native";
+import ARView from "./ARView";
 
 export default class MainScreen extends React.Component {
-    static navigationOptions = ({ navigation }) => {
-        return {
-            title: 'Model Explorer',
-            headerLeft: (
-                <Button
-                    onPress={() => navigation.navigate('Search')}
-                    title="Search"
-                />
-            ),
-        };
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "Model Explorer",
+      headerLeft: (
+        <Button onPress={() => navigation.navigate("Search")} title="Search" />
+      )
     };
+  };
 
-    render() {
-        return (
-            <MainView />
-        );
-    }
+  render() {
+    return <ARView />;
+  }
 }
